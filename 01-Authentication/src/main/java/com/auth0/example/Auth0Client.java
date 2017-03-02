f36b9bd35fdf4e36eeb4876afe7d94ee455bb248
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-03-02 15:21:18 -0300

    use spring-security-api library version 1.0.0

--- 01-Authentication/src/main/java/com/auth0/example/Auth0Client.java
+++ 01-Authentication/src/main/java/com/auth0/example/Auth0Client.java
@@ -1,31 +1,28 @@
 package com.auth0.example;
 
-import com.auth0.Auth0;
-import com.auth0.authentication.AuthenticationAPIClient;
-import com.auth0.authentication.result.UserProfile;
-import com.auth0.request.Request;
-import com.auth0.spring.security.api.Auth0JWTToken;
+import com.auth0.client.auth.AuthAPI;
+import com.auth0.exception.Auth0Exception;
+import com.auth0.json.auth.UserInfo;
+import com.auth0.spring.security.api.authentication.JwtAuthentication;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Auth0Client {
 
-    private final String clientid;
-    private final String domain;
-    private final Auth0 auth0;
-    private final AuthenticationAPIClient client;
+    private final AuthAPI client;
 
-    public Auth0Client(String clientid, String domain) {
-        this.clientid = clientid;
-        this.domain = domain;
-        this.auth0 = new Auth0(clientid, domain);
-        this.client = this.auth0.newAuthenticationAPIClient();
+    public Auth0Client(String domain, String clientId, String clientSecret) {
+        this.client = new AuthAPI(domain, clientId, clientSecret);
     }
 
-    public String getUsername(Auth0JWTToken token) {
-        final Request<UserProfile> request = client.tokenInfo(token.getJwt());
-        final UserProfile profile = request.execute();
-        return profile.getEmail();
+    public String getUserId(JwtAuthentication authentication) {
+        try {
+            UserInfo info = client.userInfo(authentication.getToken()).execute();
+            return (String) info.getValues().get("sub");
+        } catch (Auth0Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
