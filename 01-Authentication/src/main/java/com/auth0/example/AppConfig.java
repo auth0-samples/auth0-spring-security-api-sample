bc69c4c2ceaf1b10707ee51cc65ec772ad1c21df
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-03-02 19:17:22 -0300

    refactor authorization sample

--- 01-Authentication/src/main/java/com/auth0/example/AppConfig.java
+++ 01-Authentication/src/main/java/com/auth0/example/AppConfig.java
@@ -2,8 +2,8 @@ package com.auth0.example;
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,25 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity(debug = true)
 public class AppConfig extends WebSecurityConfigurerAdapter {
 
-    @Value(value = "${auth0.domain}")
-    private String domain;
-    @Value(value = "${auth0.clientId}")
-    private String clientId;
-    @Value(value = "${auth0.clientSecret}")
-    private String clientSecret;
-
     @Value(value = "${auth0.apiAudience}")
     private String apiAudience;
     @Value(value = "${auth0.issuer}")
     private String issuer;
-
-    /**
-     * Provides Auth0 API access
-     */
-    @Bean
-    public Auth0Client auth0Client() {
-        return new Auth0Client(domain, clientId, clientSecret);
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,9 +24,12 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
                 .forRS256(apiAudience, issuer)
                 .configure(http)
                 .authorizeRequests()
-                .antMatchers("/ping").permitAll()
-                .antMatchers("/secured/ping").authenticated()
-                .antMatchers();
+                .antMatchers(HttpMethod.GET, "/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/photos/**").hasAuthority("read:photos")
+                .antMatchers(HttpMethod.POST, "/photos/**").hasAuthority("create:photos")
+                .antMatchers(HttpMethod.PUT, "/photos/**").hasAuthority("update:photos")
+                .antMatchers(HttpMethod.DELETE, "/photos/**").hasAuthority("delete:photos")
+                .anyRequest().authenticated();
     }
 
 }
