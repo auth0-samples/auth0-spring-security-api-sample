da32307fe4583b3252c75cd92789b5a3329e2a44
Alexis Luque <aluque@indexlabs.com.uy>
2017-11-07 16:25:05 -0300

    Minor changes

--- 01-Authorization/exec.ps1
+++ 01-Authorization/exec.ps1
@@ -1,2 +1,2 @@
 docker build -t auth0-spring-security-mvc-api .
-docker run -p 8080:8080 -it auth0-spring-security-mvc-api
+docker run -p 3001:3001 -it auth0-spring-security-mvc-api
--- 01-Authorization/exec.sh
+++ 01-Authorization/exec.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 docker build -t auth0-spring-security-mvc-api .
-#docker run -p 3001:3001 -it auth0-spring-security-mvc-api
 docker run -p 3001:3001 -it \
        -v "$(pwd)":/usr/src/app -v ~/.test:/root/.m2 \
        auth0-spring-security-mvc-api mvn spring-boot:run
