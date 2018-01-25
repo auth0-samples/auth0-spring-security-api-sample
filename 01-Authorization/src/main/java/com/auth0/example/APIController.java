6f38252e22d6e42a2bd7ab199a9bc7504c217846
Alexis Luque <aluque@indexlabs.com.uy>
2018-01-25 16:26:30 -0300

    Return JSON response

--- 01-Authorization/src/main/java/com/auth0/example/APIController.java
+++ 01-Authorization/src/main/java/com/auth0/example/APIController.java
@@ -5,27 +5,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.json.JSONObject;
 
 @Controller
 @Component
 public class APIController {
 
-    @RequestMapping(value = "/api/public", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/public", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String publicEndpoint() {
-        return "All good. You DO NOT need to be authenticated to call /api/public";
+        return new JSONObject()
+                .put("message", "All good. You DO NOT need to be authenticated to call /api/public.")
+                .toString();
     }
 
-    @RequestMapping(value = "/api/private", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/private", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String privateEndpoint() {
-        return "All good. You can see this because you are Authenticated.";
+        return new JSONObject()
+                .put("message", "All good. You can see this because you are Authenticated.")
+                .toString();
     }
 
-    @RequestMapping(value = "/api/private-scoped", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/private-scoped", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String privateScopedEndpoint() {
-        return "All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope";
+        return new JSONObject()
+                .put("message", "All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope")
+                .toString();
     }
 
 }
